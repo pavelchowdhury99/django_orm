7f7b2a7c51fa19a116e1bb8d3d09ca6759177891
pavelchowdhury99 <59216690+pavelchowdhury99@users.noreply.github.com>
2023-01-30 23:03:59 +0530

    t

--- LearnORM/test_orm/models.py
+++ LearnORM/test_orm/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
-
+class SampleTable(models.Model):
+    char_field = models.CharField(max_length=200)
+    int_field = models.IntegerField(default=1)
+    date_field = models.DateField(default=datetime.now().date())
+    bool_field = models.BooleanField(default=True)
