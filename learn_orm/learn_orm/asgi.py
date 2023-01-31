bbfc255c5f3f3d270373c4e586d1a2dc04398d95
pavelchowdhury99 <59216690+pavelchowdhury99@users.noreply.github.com>
2023-01-31 11:27:25 +0530

    project setup

--- learn_orm/learn_orm/asgi.py
+++ learn_orm/learn_orm/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for LearnORM project.
+ASGI config for learn_orm project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LearnORM.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learn_orm.settings')
 
 application = get_asgi_application()
